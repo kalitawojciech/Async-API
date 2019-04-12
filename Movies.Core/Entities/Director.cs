17b761791fb9e04c3ebad53c6594be762f27f37e
kalitawojciech <wojtek1311997@gmail.com>
2019-04-12 11:46:17 +0200

    added entities

---
+++ Movies.Core/Entities/Director.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Movies.Core.Entities
+{
+    [Table("Directors")]
+    public class Director
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+    }
+}
---
+++ Movies.Core/Entities/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Movies.Core.Entities
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        public Guid DirectorId { get; set; }
+        public Director Director { get; set; }
+    }
+}
