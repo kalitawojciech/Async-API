3c6e9b15d2eec7d4108932ae9ee1c6ab7fa0105d
kalitawojciech <wojtek1311997@gmail.com>
2019-04-14 22:26:31 +0200

    added MoviesController and implemented method GetMovies in this controller

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private IMoviesRepository _moviesRepository;
+
+        public MoviesController(IMoviesRepository moviesRepository)
+        {
+            _moviesRepository = moviesRepository ??
+                throw new ArgumentNullException(nameof(moviesRepository));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMovies()
+        {
+            var moviesEntities = await _moviesRepository.GetMoviesAsync();
+            return Ok(moviesEntities);
+        }
+    }
+}
