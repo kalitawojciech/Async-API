25f5728fa169fa6f2238b6344a0c6c564a5e1cc5
kalitawojciech <wojtek1311997@gmail.com>
2019-04-20 22:56:40 +0200

    implemented post method in MovieCollectionsController called CreateMovieCollection

--- Movies.Api/Controllers/MovieCollectionsController.cs
+++ Movies.Api/Controllers/MovieCollectionsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Movies.Core.Interfaces;
+using Movies.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +14,18 @@ namespace Movies.Api.Controllers
     public class MovieCollectionsController : ControllerBase
     {
         private readonly IMoviesRepository _moviesRepository;
-        public MovieCollectionsController(IMoviesRepository moviesRepository)
+        private readonly IMapper _mapper;
+        public MovieCollectionsController(IMoviesRepository moviesRepository, IMapper mapper)
         {
-            _moviesRepository = moviesRepository;
+            _moviesRepository = moviesRepository ?? throw new ArgumentNullException(nameof(moviesRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovieCollection([FromBody] IEnumerable<MovieForCreation> movieCollection)
+        {
+            var movieEntities = _mapper.Map<IEnumerable<Core.Entities.Movie>>(movieCollection);
+            return Ok();
         }
     }
 }
