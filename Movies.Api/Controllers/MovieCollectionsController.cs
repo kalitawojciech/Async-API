50704ce6b83b25072b91ed6bdda637580f09266c
kalitawojciech <wojtek1311997@gmail.com>
2019-04-20 22:44:37 +0200

    created MovieCollectionsController

---
+++ Movies.Api/Controllers/MovieCollectionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Controllers
+{
+    [Route("api/moviecollections")]
+    [ApiController]
+    public class MovieCollectionsController : ControllerBase
+    {
+        private readonly IMoviesRepository _moviesRepository;
+        public MovieCollectionsController(IMoviesRepository moviesRepository)
+        {
+            _moviesRepository = moviesRepository;
+        }
+    }
+}
